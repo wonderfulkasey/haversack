32e1008cac96201772431d7e8dc406915ba17335
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-14 20:22:35 +0000

    added migration tables

--- db/migrate/20200514201919_create_items.rb
+++ db/migrate/20200514201919_create_items.rb
@@ -1,4 +1,11 @@
 class CreateItems < ActiveRecord::Migration
   def change
+    create_table :items do |t|
+      t.string  :title
+      t.string  :description
+      t.date  :date
+      t.string  :character
+      t.integer  :user_id
+    end
   end
 end
