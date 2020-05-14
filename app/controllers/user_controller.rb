119a329d5eb5ae8f03fcc3771e47f7385b0588c8
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-14 20:29:58 +0000

    added user controller

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,52 @@
+class UserController < ApplicationController
+
+ get '/signup' do
+    if !session[:user_id]
+      erb :'users/signup'
+    else
+      redirect to('/items')
+    end
+  end
+
+  post '/signup' do
+    @user = User.new(params)
+    if !@user.save
+      @errors = @user.errors.full_messages
+      erb :'users/signup'
+    else
+      session[:user_id] = @user.id
+      redirect to('/items')
+    end
+  end
+
+  get '/login' do
+    if !session[:user_id]
+      erb :'users/login'
+    else
+      redirect to('/items')
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to('/items')
+    else
+      @errors = "Invalid username or password."
+      erb :'users/login'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      @user = current_user
+      @user = nil
+      session.destroy
+      redirect to('/')
+    else
+      redirect to('/')
+    end
+  end
+
+end
