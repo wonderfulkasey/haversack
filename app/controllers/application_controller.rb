f9dec07e9ec4ee9f339f5f602ad5c1d745157760
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-14 20:26:46 +0000

    added app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,34 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get "/" do
-    erb :welcome
-  end
+  configure :development do
+      set :session_secret, 'only_for_development'
+    end
+
+    configure :production do
+      set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
+    end
+
+    get "/" do
+          if logged_in?
+            redirect to('/index')
+          else
+            erb :layout
+          end
+        end
+
+        helpers do
+
+
+          def current_user
+            @current_user ||= User.find(session[:user_id])
+          end
+
+          def logged_in?
+               !!session[:user_id]
+             end
+
+
+        end
 
 end
