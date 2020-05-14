a410468590dd97d2465eaa0ea370c098a50fab7e
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-14 20:55:53 +0000

    fixing views for signup and layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,12 +23,12 @@ class ApplicationController < Sinatra::Base
           end
         end
 
-        helpers do
+  helpers do
 
 
-          def current_user
-            @current_user ||= User.find(session[:user_id])
-          end
+  def current_user
+    @current_user ||= User.find(session[:user_id])
+  end
 
           def logged_in?
                !!session[:user_id]
