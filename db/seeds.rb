cae7b21cc5da50ea2bb0b39bedbbcffdb5cffc13
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-17 00:26:24 +0000

    fixed spelling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-caleb = User.create(user: "caleb", email: "caleb@email.com", password: "fire")
-jester = User.create(user: "jester", email: "jester@email.com", password: "ruby")
+caleb = User.create(username: "caleb", email: "caleb@email.com", password: "fire")
+jester = User.create(username: "jester", email: "jester@email.com", password: "ruby")
 
 waterskin = Item.create(title: "waterskin", description:"holds
             water", date: "4/4/4", character: "caleb")
