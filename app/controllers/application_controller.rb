562fb0ac6952c862eef4eed1a783ba5b2dab91aa
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-17 02:13:53 +0000

    changed itemcont layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,10 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
-  enable :sessions
-
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    enable :session_secret
+    enable :sessions
     set :session_secret, "critrole"
   end
 
@@ -21,14 +19,14 @@ class ApplicationController < Sinatra::Base
 
   helpers do
 
-    def logged_in?
-         !!session[:user_id]
-       end
-
-
-  def current_user
+def current_user
     @current_user ||= User.find(session[:user_id])
   end
+
+  def logged_in?
+       !!session[:user_id]
+     end
+
         end
 
 end
--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,68 +1,47 @@
 class ItemController < ApplicationController
 
+  before do
+       require_login
+   end
+
   get '/items' do
-    if logged_in?
-      @items = current_user.items
-      erb :'items/index'
-    else
-      redirect to('/login')
-    end
+    @items = Item.all
+    @items = @items.sort_by{|t| t.name}
+    erb :"items/index"
   end
 
   get '/items/new' do
-    if logged_in?
-      @current_user
       erb :'items/create_item'
-    else
-      redirect to('/login')
-    end
   end
 
   post '/items' do
-    if logged_in?
-      @item = current_user.items.build(params)
+      @item = Item.new(params)
+      @item.user_id = current_user.id
+
       if !@item.save
-        @errors = @item.errors.full_messages
-        erb :'/items/create_item'
+        current_user.items << @item
+        redirect '/items/#{@item.id}'
       else
         redirect to('/items')
       end
-    else
-      redirect to('/login')
-    end
   end
 
   get '/items/:id' do
-    @item = Item.find(params[:id])
-    if logged_in? && @item.user == current_user
-      erb :'items/show_item'
-    else
-      redirect to('/login')
-    end
+    @item = Item.find_by(id: params[:id])
+    if @item
+      @user = User.find(@recipe.user_id)
+      erb: 'items/show_item'
+  else
+     redirect "/items"
   end
 
-  get '/items/:id/edit' do
+get '/items/:id/edit' do
     @item = Item.find(params[:id])
-    if logged_in? && @item.user == current_user
-      @item = Item.find(params[:id])
-      @user = User.find(session[:user_id])
-      erb :'items/update_item'
-    else
-      redirect to('/login')
-    end
-  end
-
-  patch '/items/:id' do
-    @item = Item.find(params[:id])
-    @item.title = params[:title]
-    @item.description = params[:description]
-    @item.date = params[:date]
-    @item.character = params[:character]
-    if !@item.save
-      @errors = @item.errors.full_messages
-      erb :'/items/update_item'
-    else
-      redirect to("/items/#{@item.id}")
+    @user = User.find(@item.user_id)
+    if @item.user_id == current_user.id
+      erb :"items/edit"
+   else
+      redirect "/item/#{@item.id}"
     end
   end
 
