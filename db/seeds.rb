e9ae80c190d1e87515cc939b08119c05414ff81d
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-17 00:17:03 +0000

    fixing up the db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
 caleb = User.create(user: "caleb", email: "caleb@email.com", password: "fire")
 jester = User.create(user: "jester", email: "jester@email.com", password: "ruby")
 
-waterskin = Item.create(title: "waterskin", date: "4/4/4",
-            character: "caleb", description:"holds water")
+waterskin = Item.create(title: "waterskin", description:"holds
+            water", date: "4/4/4", character: "caleb")
 
-notebook = Item.create(title: "notebook", date: "5/5/5",
-            character: "jester", description:"sketches inside")
+notebook = Item.create(title: "notebook",
+          description:"sketches inside", date: "5/5/5",
+          character: "jester")
