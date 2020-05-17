8f18a77f09d749f41ef8c3dcd0be9ac00c0014db
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-17 00:15:28 +0000

    switch string to text in create item

--- db/migrate/20200514201919_create_items.rb
+++ db/migrate/20200514201919_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
       t.string  :title
-      t.string  :description
+      t.text  :description
       t.date  :date
       t.string  :character
       t.integer  :user_id
