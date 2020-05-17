a369b47ce75cc60c468bcfa88b05b9bb50cbe6bf
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-17 03:14:36 +0000

    added end to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,5 @@ class Item < ActiveRecord::Base
 
   belongs_to :user
 
-  validates_presence_of :title, :character, :description, :date, presence: true
+  validates_presence_of :title, :character, :description
 end
