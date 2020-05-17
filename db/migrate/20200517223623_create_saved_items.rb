2aac429774ac191acfad2110befd304ba19e3e74
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-17 22:48:46 +0000

    deleted createsaved migrate, seeded

--- db/migrate/20200517223623_create_saved_items.rb
+++
@@ -1,8 +0,0 @@
-class CreateSavedItems < ActiveRecord::Migration
-  def change
-    create_table :saved_items do |t|
-          t.integer :user_id
-          t.integer :item_id
-        end
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,6 @@ liam = User.create(username: "caleb", email: "caleb@email.com", password: "fire"
 laura = User.create(username: "jester", email: "jester@email.com", password: "ruby")
 
 
-waterskin = Item.create(title: "waterskin", user: "caleb", description: "holds water", character: "caleb"),
+waterskin = Item.create(title: "waterskin", user_id: "1", description: "holds water", character: "caleb"),
 
-notebook = Item.create(title: "notebook", user: "jester", description: "sketches inside", character: "jester")
+notebook = Item.create(title: "notebook", user_id: "2", description: "sketches inside", character: "jester")
