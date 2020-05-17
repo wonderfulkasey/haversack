a38369c1338f9de9b6d6e01e45830848258f35b6
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-17 22:07:17 +0000

    changed user controller layout

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,22 +1,21 @@
 class UserController < ApplicationController
 
- get '/signup' do
-    if !logged_in?
+  get '/signup' do
+    if !session[:user_id]
       erb :'users/signup'
     else
-      redirect "/items"
+      redirect to('/index')
     end
   end
 
   post '/signup' do
     @user = User.new(params)
-
     if !@user.save
       @errors = @user.errors.full_messages
       erb :'users/signup'
     else
       session[:user_id] = @user.id
-      redirect '/items'
+      redirect to('/index')
     end
   end
 
@@ -24,26 +23,30 @@ class UserController < ApplicationController
     if !session[:user_id]
       erb :'users/login'
     else
-      redirect '/items'
+      redirect to('/index')
     end
   end
 
   post '/login' do
     @user = User.find_by(username: params[:username])
-
-      if @user && @user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect '/items'
+      redirect to('/index')
     else
       @errors = "Invalid username or password."
-      redirect '/login'
+      erb :'users/login'
     end
   end
 
   get '/logout' do
     if logged_in?
+      @user = current_user
+      @user = nil
       session.destroy
       redirect to('/')
+    else
+      rediect to('/')
+    end
   end
-end
+
 end
