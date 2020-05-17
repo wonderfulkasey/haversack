74dd59728acf5d334d4a09c7f358fc1136051b5b
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-17 23:24:35 +0000

    changed itemcont layout back

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,79 +1,84 @@
 class ItemController < ApplicationController
 
-    before do
-        require_login
+  get '/items' do
+    if logged_in?
+      @items = current_user.items
+      erb :'items/index'
+    else
+      redirect to('/login')
+end
+end
+
+get '/items/new' do
+   if logged_in?
+     @current_user
+     erb :'items/create_item'
+   else
+     redirect to('/login')
+   end
+end
+
+
+
+     post '/items' do
+         if logged_in?
+           @item = current_user.items.build(params)
+           if !@item.save
+             @errors = @item.errors.full_messages
+             erb :'/items/create_item'
+           else
+             redirect to('/items')
+           end
+         else
+           redirect to('/login')
+         end
+end
+
+
+           get '/items/:id' do
+             @item = Item.find(params[:id])
+             if logged_in? && @item.user == current_user
+               erb :'items/show_item'
+             else
+               redirect to('/login')
+             end
+               end
+
+
+               get '/items/:id/edit' do
+             @item = Item.find(params[:id])
+             if logged_in? && @item.user == current_user
+               @item = Item.find(params[:id])
+               @user = User.find(session[:user_id])
+               erb :'items/edit_item'
+             else
+               redirect to('/login')
+             end
+               end
+
+
+               patch '/items/:id' do
+    @item = Item.find(params[:id])
+    @item.title = params[:title]
+    @item.description = params[:description]
+    @item.character = params[:character]
+    if !@item.save
+      @errors = @item.errors.full_messages
+      erb :'/items/edit_item'
+    else
+      redirect to("/items/#{@item.id}")
     end
+      end
 
-    ## CRUD-Read
-    get '/items' do
-        @items = Item.all
-        erb :"items/index"
+
+      delete '/items/:id/delete' do
+    @item = Item.find(params[:id])
+    if logged_in? && @item.user == current_user
+      @item.destroy
+      redirect to('/items')
+    else
+      redirect to('/login')
     end
-
-    ##CRUD-Create
-    get '/items/new' do
-        erb :"items/create_item"
-    end
-
-    post '/items' do
-        @item = Item.new(params)
-        @item.user_id = current_user.id
-
-        if @item.save
-            current_user.items << @item
-            redirect "/items/#{@item.id}"
-        else
-            flash[:message] = "All fields are needed to save item."
-            redirect "/items/new"
-        end
-    end
-
-    ##CRUD-Read
-    get '/items/:id' do
-        @item = Item.find_by(id: params[:id])
-        if @item
-            @user = User.find(@item.user_id)
-            erb :"items/show_item"
-        else
-            redirect "/items"
-        end
-    end
-
-    ##CRUD-Delete
-    delete '/items/:id' do
-        @item = Item.find(params[:id])
-
-        if @item.user_id == current_user.id
-            @item.delete
-            redirect "/items"
-        end
-    end
-
-    ##CRUD-Update
-    get '/items/:id/edit' do
-        @item = Item.find(params[:id])
-        @user = User.find(@item.user_id)
-        if @item.user_id == current_user.id
-            erb :"items/edit_item"
-        else
-            redirect "/items/#{@item.id}"
-        end
-    end
-
-    patch '/items/:id' do
-        @item = Item.find(params[:id])
-
-        if @item.user_id == current_user.id
-            @item.title = params[:title]
-            @item.character = params[:character]
-            @item.description = params[:description]
-
-            if @item.save
-                redirect "/items/#{@item.id}"
-            else
-                flash[:message] = "All fields are needed to save item."
-                redirect "/items/#{@item.id}/edit_item"
-            end
-        end
-    end
+      end
+end
 end
