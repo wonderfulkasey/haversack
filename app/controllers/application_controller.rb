8b5b8bc8c634988f30cb4e9b24a98a546ca07c1d
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-17 22:04:52 +0000

    redid app control layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,32 +6,27 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "critrole"
+    set :session_secret, "critrole" #ENV.fetch('SESSION_SECRET')
   end
 
-    get "/" do
-          if logged_in?
-            erb :index
-          else
-            erb :welcome
-          end
-        end
-
-        get "/index" do
-        erb :index
-          
-        end
+  get "/" do
+    if logged_in?
+      redirect to('/items')
+    else
+      erb :welcome
+    end
+  end
 
   helpers do
 
-def current_user
-    @current_user ||= User.find(session[:user_id])
+    def current_user
+      @current_user ||= User.find(session[:user_id])
+    end
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
   end
-
-  def logged_in?
-       !!session[:user_id]
-     end
-
-        end
 
 end
