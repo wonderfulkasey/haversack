0446ff33f6eb15392566d96c9cb1c9279d63738d
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-17 22:41:19 +0000

    adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 
-caleb = User.create(username: "caleb", email: "caleb@email.com", password: "fire"),
-jester = User.create(username: "jester", email: "jester@email.com", password: "ruby")
+liam = User.create(username: "caleb", email: "caleb@email.com", password: "fire"),
+laura = User.create(username: "jester", email: "jester@email.com", password: "ruby")
 
 
-waterskin = Item.create(title: "waterskin", description:"holds water", character: "caleb"),
+waterskin = Item.create(title: "waterskin", user: "caleb", description: "holds water", character: "caleb"),
 
-notebook = Item.create(title: "notebook", description:"sketches inside", character: "jester")
+notebook = Item.create(title: "notebook", user: "jester", description: "sketches inside", character: "jester")
