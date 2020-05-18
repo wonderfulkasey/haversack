e9ff303b06d6ad053afb47cb02666383787da0af
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-18 17:00:08 +0000

    fixing itemcont layout

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -11,35 +11,14 @@ end
 
 get '/items/:id/edit_item' do
   @item = Item.find(params[:id])
-if logged_in? && @item.user == current_user
-@item = Item.find(params[:id])
-@user = User.find(@item.user_id)
-if @item.user_id == current_user.id
 erb :'items/edit_item'
-else
-redirect to('/items')
-end
-else
-redirect to('/login')
-end
-end
 end
 
 patch '/items/:id' do
    @item = Item.find(params[:id])
-
-    if @item.user_id == current_user.id
-    @item.title = params[:title]
-    @item.description = params[:description]
-    @item.character = params[:character]
-
-    if !@item.save
-      @errors = @item.errors.full_messages
-      redirect to("/items/#{expense.id}")
-
-    else
-      redirect "/recipes/#{@recipe.id}/edit"
-    end
+   @item.edit_item(params["item"])
+   redirect to ("/items/#{@item.id}")
+  end
 end
 
 delete '/items/:id' do
@@ -49,29 +28,17 @@ delete '/items/:id' do
 end
 
 post '/items' do
-    if logged_in?
       @item = Item.create(params["item"])
       redirect to("/items/#{@item.id}")
-    else
-      redirect to('/login')
-    end
 end
 
 get '/items/new' do
-   if logged_in?
-     @current_user
-     erb :'items/create_item'
-   else
-     redirect to('/login')
-   end
+   erb :'items/create_item'
 end
 
 get '/items/:id' do
    @item = Item.find_by(id:params[:id])
-if logged_in? && @item.user == current_user
      erb :'items/show_item'
 
-   else
-     redirect to('/login')
-   end
+
      end
