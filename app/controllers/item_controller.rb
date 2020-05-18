e8f514afb440474d3d1e4921560ac16abab42670
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-18 16:16:36 +0000

    trying to fix id edit

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -51,38 +51,38 @@ end
 
 
                get '/items/:id/edit_item' do
-             @item = Item.find(params[:id])
              if logged_in? && @item.user == current_user
                @item = Item.find(params[:id])
                @user = User.find(@item.user_id)
-             #if @item.user_id == current_user.id
+             if @item.user_id == current_user.id
                erb :'items/edit_item'
-             #else
-            #   redirect to('/items')
-            # end
+             else
+              redirect to('/items')
+            end
              else
                redirect to('/login')
              end
                end
+             end
 
 patch '/items/:id' do
    @item = Item.find(params[:id])
 
-    #if @item.user_id == current_user.id
+    if @item.user_id == current_user.id
     @item.title = params[:title]
     @item.description = params[:description]
     @item.character = params[:character]
 
     if !@item.save
       @errors = @item.errors.full_messages
-      erb :'/items/edit_item'
-    else
       redirect to("/items/#{expense.id}")
+
+    else
+      redirect "/recipes/#{@recipe.id}/edit"
     end
-      end
+end
 
-
-delete '/items/:id/delete' do
+delete '/items/:id' do
     @item = Item.find(params[:id])
     if logged_in? && @item.user == current_user
       @item.destroy
