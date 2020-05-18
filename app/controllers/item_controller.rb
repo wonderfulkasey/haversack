aa286c649f50c393928455878b2d8b28598b3464
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-18 16:09:37 +0000

    changing itemcont layout

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -37,16 +37,13 @@ end
          end
 end
 
+
+
           get '/items/:id' do
              @item = Item.find_by(id:params[:id])
     if logged_in? && @item.user == current_user
                erb :'items/show_item'
 
-               if @item
-                 @user = User.find(@item.user_id)
-               else
-              redirect to('/items')
-             end
              else
                redirect to('/login')
              end
@@ -58,21 +55,20 @@ end
              if logged_in? && @item.user == current_user
                @item = Item.find(params[:id])
                @user = User.find(@item.user_id)
-             if @item.user_id == current_user.id
+             #if @item.user_id == current_user.id
                erb :'items/edit_item'
-             else
-               redirect to('/items')
-             end
+             #else
+            #   redirect to('/items')
+            # end
              else
                redirect to('/login')
              end
                end
 
-
-patch '/items' do
+patch '/items/:id' do
    @item = Item.find(params[:id])
 
-    if @item.user_id == current_user.id
+    #if @item.user_id == current_user.id
     @item.title = params[:title]
     @item.description = params[:description]
     @item.character = params[:character]
@@ -81,10 +77,10 @@ patch '/items' do
       @errors = @item.errors.full_messages
       erb :'/items/edit_item'
     else
-      redirect to("/items")
+      redirect to("/items/#{expense.id}")
     end
       end
-end
+
 
 delete '/items/:id/delete' do
     @item = Item.find(params[:id])
