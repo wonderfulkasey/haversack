387883a96df990e2a63421c3a1a51053c3e39f30
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-22 16:11:08 +0000

    fixing layout

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -11,39 +11,41 @@ class UserController < ApplicationController
   post '/signup' do
     @user = User.new(params)
 
-    if !@user.save
-      @errors = @user.errors.full_messages
-      erb :'users/signup'
-    else
-      session[:user_id] = @user.id
-      redirect '/items'
+            if !@user.save
+              @errors = @user.errors.full_messages
+              erb :'users/signup'
+            else
+              session[:user_id] = @user.id
+              redirect '/items'
     end
   end
 
   get '/login' do
-    if !session[:user_id]
-      erb :'users/login'
-    else
-      redirect '/items'
+            if !session[:user_id]
+              erb :'users/login'
+            else
+              redirect '/items'
     end
   end
 
   post '/login' do
     @user = User.find_by(username: params[:username])
 
-      if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect '/items'
-    else
-      @errors = "Invalid username or password."
-      redirect '/login'
+            if @user && @user.authenticate(params[:password])
+              session[:user_id] = @user.id
+              redirect '/items'
+            else
+              @errors = "Invalid username or password."
+              redirect '/login'
     end
   end
 
   get '/logout' do
-    if logged_in?
-      session.destroy
-      redirect to('/')
-  end
+            if logged_in?
+              session.destroy
+              redirect to('/')
+
+            #else?????
+          end
 end
 end
