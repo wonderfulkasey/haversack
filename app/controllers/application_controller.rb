1f8d309769c6e964b55e5eb1a3fa1fca8dcf466d
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-26 17:44:14 +0000

    error appcont

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require './config/environment'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
 
@@ -6,6 +7,7 @@ configure do
     set :root, File.join(File.dirname(__FILE__), '..')
     set :views, File.join(root, "views")
     enable :sessions
+    use Rack::Flash
     set :session_secret, "critrole"
     set :method_override, true
 end
@@ -27,5 +29,11 @@ end
 def logged_in?
       !!session[:user_id]
 end
+
+def require_login
+    unless logged_in?
+      redirect "/login"
+    end
+  end
 end
 end
